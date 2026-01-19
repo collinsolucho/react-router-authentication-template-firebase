72067cbe9146af64ce02523fc08b26545112b4d4
collis olucho <collinsolucho@gmail.com>
2026-01-19 14:57:30 +0300

    added helpers to save users to  db

--- model/database.js
+++ model/database.js
@@ -1,4 +1,17 @@
 import { client } from "../.server/mongo";
 
 let db = client.db("passwordAuthentication");
-let accounts = db.collection("logins");
+let logins = db.collection("logins");
+
+export async function updateUserByEmail(email, userData) {
+  return await logins.updateOne(
+    { email: email }, // Filter: find user by email
+    {
+      $set: {
+        ...userData,
+        lastLogin: new Date(),
+      },
+    },
+    { upsert: true } // 👈 This is the "Magic" for manual add
+  );
+}
